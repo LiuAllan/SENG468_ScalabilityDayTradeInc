71f462ebb14790682cf660c823ee04c3b10c2321
ParmbeerJohal <parmbeerjohal@gmail.com>
2020-01-31 01:10:08 -0800

    used 1userworkload file to successfully test audit.py. Now need input from transaction server

--- database/audit.py
+++ database/audit.py
@@ -23,7 +23,7 @@ def quoteServer(usr, t = time.time(), server = 'CLT1', transnum = 1, qtime = tim
     string += "        <stockSymbol>" + str(symbol) + "</stockSymbol>\n"
     string += "        <price>" + str(price) + "</price>\n"
     string += "        <cryptokey>" + str(key) + "</cryptokey>\n"
-    string = "    </quoteServer>\n"
+    string += "    </quoteServer>\n"
     return string
 
 def accountTransaction(action, usr, funds, t = time.time(), server = 'CLT1', transnum = 1):
@@ -34,7 +34,7 @@ def accountTransaction(action, usr, funds, t = time.time(), server = 'CLT1', tra
     string += "        <action>" + str(action) + "</action>\n"
     string += "        <username>" + str(usr) + "</username>\n"
     string += "        <funds>" + str(funds) + "</funds>\n"
-    string = "    </accountTransaction>\n"
+    string += "    </accountTransaction>\n"
     return string
 
 def systemEvent(t, server, transnum, cmd, usr, symbol, funds):
@@ -46,7 +46,7 @@ def systemEvent(t, server, transnum, cmd, usr, symbol, funds):
     string += "        <username>" + str(usr) + "</username>\n"
     string += "        <stockSymbol>" + str(symbol) + "</stockSymbol>\n"
     string += "        <funds>" + str(funds) + "</funds>\n"
-    string = "    </systemEvent>\n"
+    string += "    </systemEvent>\n"
     return string
 
 def errorEvent(t, server, transnum, cmd, usr, symbol, funds, msg):
@@ -59,7 +59,7 @@ def errorEvent(t, server, transnum, cmd, usr, symbol, funds, msg):
     string += "        <stockSymbol>" + str(symbol) + "</stockSymbol>\n"
     string += "        <funds>" + str(funds) + "</funds>\n"
     string += "        <errorMessage>" + str(msg) + "</errorMessage>\n"
-    string = "    </errorEvent>\n"
+    string += "    </errorEvent>\n"
     return string
 
 def debugEvent(t, server, transnum, cmd, usr, symbol, funds, msg):
@@ -72,7 +72,7 @@ def debugEvent(t, server, transnum, cmd, usr, symbol, funds, msg):
     string += "        <stockSymbol>" + str(symbol) + "</stockSymbol>\n"
     string += "        <funds>" + str(funds) + "</funds>\n"
     string += "        <debugMessage>" + str(msg) + "</debugMessage>\n"
-    string = "    </debugEvent>\n"
+    string += "    </debugEvent>\n"
     return string
 
 
@@ -90,20 +90,26 @@ def main():
     # Creates a list of list which each line is broken into parameters
     contents = [re.sub("\[[0-9]+] ", "", item.strip()).split(",") for item in file.readlines()]
     file.close()
-    
-    log = ""
+    print(contents)
+    usr_funds = 0.00
 
+    buy_amount = 0.00
+    sell_amount = 0.00
+
+    log = ""
     for line in contents:
         if(line[0] == "ADD"):
             print("ADD")
-            log += userCommand(line[0], line[1], line[2])
-            log += accountTransaction(action = 'add', usr = line[1], funds = line[2])
+            usr_funds += float(line[2])
+            log += userCommand(line[0], line[1], usr_funds)
+            log += accountTransaction(action = 'add', usr = line[1], funds = usr_funds)
         elif (line[0] == "QUOTE"):
             print("QUOTE")
             log += quoteServer(usr = line[1])
         elif (line[0] == "BUY"):
             print("BUY")
-            #log += userCommand(line[0], line[1], line[2])
+            buy_amount += float(line[3])
+            #log += userCommand(line[0], line[1], usr_funds)
             #log += systemEvent()
             #log += quoteServer()
             #log += accountTransaction()
@@ -113,32 +119,55 @@ def main():
             #log += systemEvent()
         elif (line[0] == "CANCEL_BUY"):
             print("CANCEL_BUY")
+            #log += userCommand()
+            #log += systemEvent()
         elif (line[0] == "SELL"):
             print("SELL")
+            #log += userCommand(line[0], line[1], usr_funds)
+            #log += systemEvent()
+            #log += quoteServer()
+            #log += accountTransaction()
         elif (line[0] == "COMMIT_SELL"):
             print("COMMIT_SELL")
+            #log += userCommand(line[0], line[1], line[2])
+            #log += systemEvent()
         elif (line[0] == "CANCEL_SELL"):
             print("CANCEL_SELL")
+            #log += userCommand(line[0], line[1], line[2])
+            #log += systemEvent()
         elif (line[0] == "SET_BUY_AMOUNT"):
             print("SET_BUY_AMOUNT")
+            #log += userCommand(line[0], line[1], line[2])
+            #log += systemEvent()
         elif (line[0] == "CANCEL_SET_BUY"):
             print("CANCEL_SET_BUY")
+            #log += userCommand(line[0], line[1], line[2])
+            #log += systemEvent()
         elif (line[0] == "SET_BUY_TRIGGER"):
             print("SET_BUY_TRIGGER")
+            #log += userCommand(line[0], line[1], line[2])
+            #log += systemEvent()
         elif (line[0] == "SET_SELL_AMOUNT"):
             print("SET_SELL_AMOUNT")
+            #log += userCommand(line[0], line[1], line[2])
+            #log += systemEvent()
         elif (line[0] == "SET_SELL_TRIGGER"):
             print("SET_SELL_TRIGGER")
+            #log += userCommand(line[0], line[1], line[2])
+            #log += systemEvent()
         elif (line[0] == "CANCEL_SET_SELL"):
             print("CANCEL_SET_SELL")
+            #log += userCommand(line[0], line[1], line[2])
+            #log += systemEvent()
         elif (line[0] == "DUMPLOG"):
             print("DUMPLOG")
+            log_string = '<?xml version="1.0"?>\n<log>\n\n' + log + '\n</log>'
+            xml_file = open(line[1], "w")
+            xml_file.write(log_string)
         elif (line[0] == "DISPLAY_SUMMARY"):
             print("DISPLAY_SUMMARY")
+            #log += userCommand(line[0], line[1], line[2])
 
-    log_string = '<?xml version="1.0"?>\n<log>\n\n' + log + '\n</log>'
-    xml_file = open("log.xml", "w")
-    xml_file.write(log_string)
     print(log_string)
 	
 
