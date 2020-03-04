ad26237df4f034a927c7cbf41e3f928f5efea4aa
ParmbeerJohal <49958791+ParmbeerJohal@users.noreply.github.com>
2020-03-04 15:01:24 -0800

    altered schema to satisfy xml schema

--- database/audit.py
+++ database/audit.py
@@ -7,7 +7,7 @@ def userCommand(item):
     string = "    <userCommand>\n"
     string += "        <timestamp>" + str(item[3]) + "</timestamp>\n"
     string += "        <server>" + str(item[8]) + "</server>\n"
-    string += "        <transactionNum>" + str(item[0]) + "<transactionNum>\n"
+    string += "        <transactionNum>" + str(item[0]) + "</transactionNum>\n"
     string += "        <command>" + str(item[2]) + "</command>\n"
     string += "        <username>" + str(item[1]) + "</username>\n"
     string += "        <funds>" + str(item[6]) + "</funds>\n"
@@ -18,7 +18,7 @@ def quoteServer(item):
     string = "    <quoteServer>\n"
     string += "        <timestamp>" + str(item[3]) + "</timestamp>\n"
     string += "        <server>" + str(item[8]) + "</server>\n"
-    string += "        <transactionNum>" + str(item[0]) + "<transactionNum>\n"
+    string += "        <transactionNum>" + str(item[0]) + "</transactionNum>\n"
     string += "        <quoteServerTime>" + str(item[11]) + "</quoteServerTime>\n"
     string += "        <username>" + str(item[1]) + "</username>\n"
     string += "        <stockSymbol>" + str(item[4]) + "</stockSymbol>\n"
@@ -31,10 +31,10 @@ def accountTransaction(item):
     string = "    <accountTransaction>\n"
     string += "        <timestamp>" + str(item[3]) + "</timestamp>\n"
     string += "        <server>" + str(item[8]) + "</server>\n"
-    string += "        <transactionNum>" + str(item[0]) + "<transactionNum>\n"
+    string += "        <transactionNum>" + str(item[0]) + "</transactionNum>\n"
     string += "        <action>" + str(item[2]) + "</action>\n"
     string += "        <username>" + str(item[1]) + "</username>\n"
-    string += "        <funds>" + str(item[5]) + "</funds>\n"
+    string += "        <funds>" + str(item[6]) + "</funds>\n"
     string += "    </accountTransaction>\n"
     return string
 
@@ -42,7 +42,7 @@ def systemEvent(item):
     string = "    <systemEvent>\n"
     string += "        <timestamp>" + str(item[3]) + "</timestamp>\n"
     string += "        <server>" + str(item[8]) + "</server>\n"
-    string += "        <transactionNum>" + str(item[0]) + "<transactionNum>\n"
+    string += "        <transactionNum>" + str(item[0]) + "</transactionNum>\n"
     string += "        <command>" + str(item[2]) + "</command>\n"
     string += "        <username>" + str(item[1]) + "</username>\n"
     string += "        <stockSymbol>" + str(item[4]) + "</stockSymbol>\n"
@@ -54,7 +54,7 @@ def errorEvent(item):
     string = "    <errorEvent>\n"
     string += "        <timestamp>" + str(item[3]) + "</timestamp>\n"
     string += "        <server>" + str(item[8]) + "</server>\n"
-    string += "        <transactionNum>" + str(item[0]) + "<transactionNum>\n"
+    string += "        <transactionNum>" + str(item[0]) + "</transactionNum>\n"
     string += "        <command>" + str(item[2]) + "</command>\n"
     string += "        <username>" + str(item[1]) + "</username>\n"
     string += "        <stockSymbol>" + str(item[4]) + "</stockSymbol>\n"
@@ -67,7 +67,7 @@ def debugEvent(item):
     string = "    <debugEvent>\n"
     string += "        <timestamp>" + str(item[3]) + "</timestamp>\n"
     string += "        <server>" + str(item[8]) + "</server>\n"
-    string += "        <transactionNum>" + str(item[0]) + "<transactionNum>\n"
+    string += "        <transactionNum>" + str(item[0]) + "</transactionNum>\n"
     string += "        <command>" + str(item[2]) + "</command>\n"
     string += "        <username>" + str(item[1]) + "</username>\n"
     string += "        <stockSymbol>" + str(item[4]) + "</stockSymbol>\n"
@@ -97,7 +97,7 @@ def main():
             print('line received is ', line)
 			
             redirect(line)
-            audit_print = redirect(line)
+            #audit_print = redirect(line)
             #c.send(audit_print.encode())
 			
             c.close()
@@ -120,13 +120,52 @@ def redirect(line):
         if(item[2] == "ADD"):
             print("ADD")
             log += userCommand(item)
-            log += accountTransaction(item)
+            if item[13] is not None:
+                log += errorEvent(item)
+            elif item[14] is not None:
+                log += debugEvent(item)
+            else:
+                log += accountTransaction(item)
+        #ADD ENDS
+
         elif (item[2] == "QUOTE"):
             print("QUOTE")
-            log += quoteServer(item)
+            log += userCommand(item)
+            if item[13] is not None:
+                log += errorEvent(item)
+            elif item[14] is not None:
+                log += debugEvent(item)
+            else:
+                log += quoteServer(item)
+        #QUOTE END
+
         elif (item[2] == "BUY"):
             print("BUY")
-            #buy_amount += float(line[3])
+            log += userCommand(item)
+            if item[13] is not None:
+                log += errorEvent(item)
+            elif item[14] is not None:
+                log += debugEvent(item)
+            else:
+                log += quoteServer(item)
+                log += accountTransaction(item)
+                log += systemEvent(item)
+        #BUY ENDS
+
+        elif (item[2] == "COMMIT_BUY"):
+            print("COMMIT_BUY")
+            log += userCommand(item)
+            if item[13] is not None:
+                log += errorEvent(item)
+            elif item[14] is not None:
+                log += debugEvent(item)
+            else:
+                log += accountTransaction(item)
+                log += systemEvent(item)
+        #COMMIT_BUY ENDS
+
+        elif (item[2] == "CANCEL_BUY"):
+            print("CANCEL_BUY")
             log += userCommand(item)
             if item[13] is not None:
                 log += errorEvent(item)
@@ -134,69 +173,135 @@ def redirect(line):
                 log += debugEvent(item)
             else:
                 log += systemEvent(item)
-                log += quoteServer(item)
-                log += accountTransaction(item)
-        elif (item[2] == "COMMIT_BUY"):
-            print("COMMIT_BUY")
-            #log += userCommand(item)
-            #log += systemEvent(item)
-        elif (item[2] == "CANCEL_BUY"):
-            print("CANCEL_BUY")
-            #log += userCommand(item)
-            #log += systemEvent(item)
+        #CANCEL_BUY ENDS
+
         elif (item[2] == "SELL"):
             print("SELL")
-            #log += userCommand(item)
-            #log += systemEvent(item)
-            #log += quoteServer(item)
-            #log += accountTransaction(item)
+            log += userCommand(item)
+            if item[13] is not None:
+                log += errorEvent(item)
+            elif item[14] is not None:
+                log += debugEvent(item)
+            else:
+                log += quoteServer(item)
+                log += accountTransaction(item)
+                log += systemEvent(item)
+        #SELL ENDS
+
         elif (item[2] == "COMMIT_SELL"):
             print("COMMIT_SELL")
-            #log += userCommand(item)
-            #log += systemEvent(item)
+            log += userCommand(item)
+            if item[13] is not None:
+                log += errorEvent(item)
+            elif item[14] is not None:
+                log += debugEvent(item)
+            else:
+                log += accountTransaction(item)
+                log += systemEvent(item)
+        #COMMIT_SELL ENDS
+
         elif (item[2] == "CANCEL_SELL"):
             print("CANCEL_SELL")
-            #log += userCommand(item)
-            #log += systemEvent(item)
+            log += userCommand(item)
+            if item[13] is not None:
+                log += errorEvent(item)
+            elif item[14] is not None:
+                log += debugEvent(item)
+            else:
+                log += systemEvent(item)
+        #CANCEL_SELL ENDS
+
         elif (item[2] == "SET_BUY_AMOUNT"):
             print("SET_BUY_AMOUNT")
-            #log += userCommand(item)
-            #log += systemEvent(item)
+            log += userCommand(item)
+            if item[13] is not None:
+                log += errorEvent(item)
+            elif item[14] is not None:
+                log += debugEvent(item)
+            else:
+                log += accountTransaction(item)
+                log += systemEvent(item)
+        #SET_BUY_AMOUNT ENDS
+
         elif (item[2] == "CANCEL_SET_BUY"):
             print("CANCEL_SET_BUY")
-            #log += userCommand(item)
-            #log += systemEvent(item)
+            log += userCommand(item)
+            if item[13] is not None:
+                log += errorEvent(item)
+            elif item[14] is not None:
+                log += debugEvent(item)
+            else:
+                log += systemEvent(item)
+        #CANCEL_SET_BUY ENDS
+
         elif (item[2] == "SET_BUY_TRIGGER"):
             print("SET_BUY_TRIGGER")
-            #log += userCommand(item)
-            #log += systemEvent(item)
+            log += userCommand(item)
+            if item[13] is not None:
+                log += errorEvent(item)
+            elif item[14] is not None:
+                log += debugEvent(item)
+            else:
+                log += systemEvent(item)
+        #SET_BUY_TRIGGER ENDS
+
         elif (item[2] == "SET_SELL_AMOUNT"):
             print("SET_SELL_AMOUNT")
-            #log += userCommand(item)
-            #log += systemEvent(item)
+            log += userCommand(item)
+            if item[13] is not None:
+                log += errorEvent(item)
+            elif item[14] is not None:
+                log += debugEvent(item)
+            else:
+                log += systemEvent(item)
+        #SET_SELL_AMOUNT ENDS
+
         elif (item[2] == "SET_SELL_TRIGGER"):
             print("SET_SELL_TRIGGER")
-            #log += userCommand(item)
-            #log += systemEvent(item)
+            log += userCommand(item)
+            if item[13] is not None:
+                log += errorEvent(item)
+            elif item[14] is not None:
+                log += debugEvent(item)
+            else:
+                log += accountTransaction(item)
+                log += systemEvent(item)
+        #SET_SELL_TRIGGER ENDS
+
         elif (item[2] == "CANCEL_SET_SELL"):
             print("CANCEL_SET_SELL")
-            #log += userCommand(item)
-            #log += systemEvent(item)
+            log += userCommand(item)
+            if item[13] is not None:
+                log += errorEvent(item)
+            elif item[14] is not None:
+                log += debugEvent(item)
+            else:
+                log += systemEvent(item)
+        #CANCEL_SET_SELL ENDS
+
         elif (item[2] == "DISPLAY_SUMMARY"):
             print("DISPLAY_SUMMARY")
-            #log += userCommand(item)
-        elif (item[2] == "DUMPLOG"):
-            print("DUMPLOG")
-            log_string = '<?xml version="1.0"?>\n<log>\n\n' + log + '\n</log>'
-            xml_file = open(line[1], "w")
-            xml_file.write(log_string)
+            log += userCommand(item)
+            if item[13] is not None:
+                log += errorEvent(item)
+            elif item[14] is not None:
+                log += debugEvent(item)
+            else:
+                log += systemEvent(item)
+        #DISPLAY_SUMMARY ENDS
+
+        #elif (item[2] == "DUMPLOG"):
+            #print("DUMPLOG")
+            #log_string = '<?xml version="1.0"?>\n<log>\n\n' + log + '\n</log>'
+            #xml_file = open(line[1], "w")
+            #xml_file.write(log_string)
 
     log_string = '<?xml version="1.0"?>\n<log>\n\n' + log + '\n</log>'
-    xml_file = open('log.xml', "w")
+    xml_file = open('TESTLOG.xml', "w")
     xml_file.write(log_string)
 
     #print(log_string)
-    print(log_string)
+    #print(log_string)
 	
 
 
